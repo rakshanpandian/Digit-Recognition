cdf078b0442fc10637d9bc13a1b5e12c43c2b761
Pranav <Gonarchov@outlook.com>
2026-04-09 23:45:51 +0530

    Update model_train.py
    Final version - changed to MNIST

--- model_train.py
+++ model_train.py
@@ -5,10 +5,12 @@ from sklearn.decomposition import PCA
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.datasets import fetch_openml
 
-MODEL_FILE = "digit_rf_mnist_perfect.pkl"
+MODEL_FILE = "digit_rf_mnist.pkl"
+
 
 def load_or_train_model():
     if not os.path.exists(MODEL_FILE):
+        print("Downloading MNIST...")
 
         mnist = fetch_openml('mnist_784', version=1, as_frame=False)
         X = mnist.data.astype(np.float32) / 255.0
@@ -27,8 +29,6 @@ def load_or_train_model():
         rf.fit(X_pca, y)
 
         joblib.dump((pca, rf), MODEL_FILE)
-        print("GUI.py")
-    else:
-        print("Loading existing model...")
+        print("Model saved.")
 
     return joblib.load(MODEL_FILE)
